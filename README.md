7ab9fd63f875647d3e5cb27cd0f50c12f2492f91
catalinmiron <mironcatalin@gmail.com>
2015-06-30 01:12:15 +0300

    Add live demo links and modify the index.html

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 WeatheReact app
 -----
 
+Live demo:
+
+- [Surge.sh](http://weather.surge.sh/)
+- [Github pages](http://catalinmiron.github.io/weathereact/)
+
 Another weather app build with Reactjs usign [Open Weather API](http://openweathermap.org/api)
 
 ![weathereact-app-update](https://cloud.githubusercontent.com/assets/2805320/8419117/164a78a2-1ec0-11e5-990d-1611c909d5ec.gif)
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,21 @@ var getConfig = require('hjs-webpack');
 
 module.exports = getConfig({
   in: 'src/app.js',
-  out: 'dist/',
-  clearBeforeBuild: true
+  out: 'dist',
+  clearBeforeBuild: true,
+  html: function (data) {
+    return {
+      'index.html': [
+        '<html>',
+          '<head>',
+            '<link href="' + data.css + '" rel="stylesheet" type="text/css" />',
+          '</head>',
+          '<body>',
+            '<h1>Home Page</h1>',
+            '<script src="' + data.main + '"></script>',
+          '</body>',
+        '</html>'
+      ].join('')
+    }
+  }
 });
