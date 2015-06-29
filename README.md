9a19586d796ee6fa2fc81f0f474ae05b82cf2bd4
catalin-miron-hs <catalin.miron@hootsuite.com>
2015-06-29 18:34:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+WeatheReact app
+-----
+
+Another weather app build with Reactjs usign [Open Weather API](http://openweathermap.org/api)
+
+![weathereact_app](https://cloud.githubusercontent.com/assets/2805320/8411462/bcf3e47e-1e8c-11e5-80f7-3ffb49509b8c.gif)
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var getConfig = require('hjs-webpack');
+
+
+module.exports = getConfig({
+  // entry point for the app
+  in: 'src/app.js',
+
+  // Name or full path of output directory
+  // commonly named `www` or `public`. This
+  // is where your fully static site should
+  // end up for simple deployment.
+  out: 'public',
+
+  // This will destroy and re-create your
+  // `out` folder before building so you always
+  // get a fresh folder. Usually you want this
+  // but since it's destructive we make it
+  // false by default
+  clearBeforeBuild: true
+});
