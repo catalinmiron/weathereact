4f2608e5891244742c9ffb20c50d568d9ead7fb2
catalin-miron-hs <catalin.miron@hootsuite.com>
2015-06-30 12:49:17 +0300

    Add more details to html index

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,11 @@ module.exports = getConfig({
   html: function (data) {
     return {
       'index.html': [
-        '<html>',
+        '<!DOCTYPE html>',
+        '<html lang="en">',
           '<head>',
+            '<meta charset="UTF-8">',
+            '<meta name="viewport" content="width=device-width, initial-scale=1">',
             '<link href="' + data.css + '" rel="stylesheet" type="text/css" />',
           '</head>',
           '<body>',
