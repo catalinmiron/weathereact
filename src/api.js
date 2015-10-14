6e45be7b5189bc79a87851e127cb1c01638ea263
catalin-miron <catalin.miron@hootsuite.com>
2015-10-14 13:30:12 +0300

    Add an API_KEY

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
+import {API_KEY} from './../secrets';
+
 var fetchWeather = function(city, metric) {
-  let url = `http://api.openweathermap.org/data/2.5/find?q=${city}&units=${metric}`;
+  let url = `http://api.openweathermap.org/data/2.5/find?q=${city}&units=${metric}&appid=${API_KEY}`;
 
   return fetch(url).then((response) => response.json());
 }
